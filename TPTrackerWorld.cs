3778489a4f65cd37c80d3cf8c3fdc37334d7fc00
Vince Kálmán <30904009+DRKV333@users.noreply.github.com>
2017-09-22 20:06:23 +0200

    Added teleporter tracer.

--- TPTrackerWorld.cs
+++ TPTrackerWorld.cs
@@ -4,14 +4,17 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 using TPUnchained.Tiles;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
 
 namespace TPUnchained
 {
-    class TPTrackerWrold : ModWorld
+    class TPTrackerWorld : ModWorld
     {
         public List<TEWirelessTeleporter> teleporters = new List<TEWirelessTeleporter>();
 
@@ -52,6 +55,35 @@ namespace TPUnchained
                 TileEntity te;
                 if (TileEntity.ByID.TryGetValue(reader.ReadInt32(), out te))
                     teleporters.Add((TEWirelessTeleporter)te);
+            }
+        }
+
+        
+
+        public override void PostUpdate()
+        {
+            if (Main.tileFrame[mod.TileType<WirelessTeleporterTile>()] == 0 && Main.tileFrameCounter[mod.TileType<WirelessTeleporterTile>()] == 0)
+            {
+                foreach (var item in teleporters)
+                {
+                    Vector2 thisPos = new Vector2(item.Position.X * 16 + 8, item.Position.Y * 16 + 8);
+
+                    if(item.Next != -1)
+                    {
+                        TileEntity next = TileEntity.ByID[item.Next];
+                        Vector2 nextPos = new Vector2(next.Position.X * 16 + 8, next.Position.Y * 16 + 8);
+
+                        Dust dust = Dust.NewDustPerfect(thisPos, mod.DustType<TracerDust>(), (nextPos - thisPos) / 10);
+                    }
+                    
+                    if (item.Prev != -1)
+                    {
+                        TileEntity prev = TileEntity.ByID[item.Prev];
+                        Vector2 prevPos = new Vector2(prev.Position.X * 16 + 8, prev.Position.Y * 16 + 8);
+
+                        Dust dust = Dust.NewDustPerfect(prevPos, mod.DustType<TracerDust>(), (thisPos - prevPos) / 10);
+                    }
+                }
             }
         }
     }
